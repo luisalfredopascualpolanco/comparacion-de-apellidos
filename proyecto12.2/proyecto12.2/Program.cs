969a4477032679c11a6c6ccdba34409ebc7eae2d
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2022-01-04 21:58:44 -0800

    version1.0

---
+++ proyecto12.2/proyecto12.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string apellido1, apellido2;
+
+                Console.Write("Introduce un apellido: ");
+                apellido1 = Console.ReadLine().ToLower();
+
+                Console.Write("Introduce otro apellido: ");
+                apellido2 = Console.ReadLine().ToLower();
+
+                if (apellido1 == apellido2)
+                {
+                    Console.WriteLine("Los apellidos son iguales.");
+                }
+                else
+                {
+                    Console.WriteLine("Los apellidos son distintos.");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un dato invalido.");                
+            }
+            Console.ReadKey();
+        }
+    }
+}
